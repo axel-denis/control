0211093f46eb4d519884b751e0586bd8f5ae984c
Axel Denis <axel.denis@epitech.eu>
2025-07-30 13:32:03 +0200

    feat: added jellyfin

---
+++ modules/jellyfin.nix
@@ -0,0 +1,54 @@
+{ config, lib, ... }:
+
+with lib;
+
+let cfg = config.myhomeserver.jellyfin;
+in {
+  options.myhomeserver.jellyfin = {
+    enable = mkEnableOption "Enable Jellyfin";
+
+    version = mkOption {
+      type = types.string;
+      default = "release";
+      defaultText = "release";
+      description = "Version name to use for Jellyfin images";
+    };
+
+    rootPath = mkOption {
+      type = types.path;
+      description = "Root path for Jellyfin media and appdata (required)";
+    };
+
+    pathOverride = {
+      media = mkInheritedPathOption {
+        parentName = "rootPath";
+        parent = cfg.rootPath;
+        defaultSubpath = "media";
+        description = "Path for Jellyfin media (movies).";
+      };
+
+      config = mkInheritedPathOption {
+        parentName = "rootPath";
+        parent = cfg.rootPath;
+        defaultSubpath = "config";
+        description = "Path for Jellyfin appdata (config).";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+    virtualisation.oci-containers.containers = {
+      jellyfin = {
+        image = "jellyfin/jellyfin:latest";
+        ports = [ "8096:8096" ];
+        volumes = [
+          #"${jellyfinRoot}/media:/media"
+          "/mnt/films/:/media"
+          "${jellyfinRoot}/config:/config"
+        ];
+      };
+    };
+  };
+}
+
