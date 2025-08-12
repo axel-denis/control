f8727b3a3565200f87c7a4e608a66f8f6f1148a3
Axel Denis <axel.denis@epitech.eu>
2025-08-12 20:32:52 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,15 @@
   description = "Home Server Service Modules";
 
   outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      lib = import ./lib { inherit (pkgs) lib; };
-    in {
-      nixosModules.default = { lib, ... }@args:
-        let inherit (args) config pkgs;
-        in import [
-          ./modules/transmission.nix
-          ./modules/immich.nix
-          ./modules/jellyfin.nix
-        ] { inherit lib config pkgs; };
-    };
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    nixosModules.default = [
+      ./modules/transmission.nix
+      ./modules/immich.nix
+      ./modules/jellyfin.nix
+    ];
+    lib = import ./lib { inherit (pkgs) lib; };
+  };
 }
