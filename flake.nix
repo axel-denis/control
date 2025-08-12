daf08bab5f637f91349d75eda343510cb8ac48c2
Axel Denis <axel.denis@epitech.eu>
2025-08-12 15:53:11 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      lib = import "./lib" { inherit (pkgs) lib; };
+      lib = import ./lib { inherit (pkgs) lib; };
     in {
       nixosModules.default = { lib, ... }@args:
         let inherit (args) config pkgs;
-        in import "./modules" { inherit lib config pkgs; };
+        in import ./modules { inherit lib config pkgs; };
     };
 }
