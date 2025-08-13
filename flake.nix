9967ae41c78b229262ba3c4f8b6965da1d4e16a6
Axel Denis <axel.denis@epitech.eu>
2025-08-13 12:14:21 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    modules = [
-      ./modules/transmission.nix
-      ./modules/immich.nix
-      ./modules/jellyfin.nix
-    ];
+    nixosModules = {
+      transmission = ./modules/transmission.nix;
+      immich = ./modules/immich.nix;
+      jellyfin = ./modules/jellyfin.nix;
+    };
     lib = import ./lib { inherit (pkgs) lib; };
   };
 }
