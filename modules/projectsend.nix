899fa641b50851e2622d39a6b185a8357ce3015c
Axel DENIS <axel.denis@epitech.eu>
2025-09-20 23:11:27 +0200

    feat: added projectsend

---
+++ modules/projectsend.nix
@@ -0,0 +1,73 @@
+{ config, helpers, lib, ... }:
+
+with lib;
+let cfg = config.homeserver.projectsend;
+in {
+  options.homeserver.projectsend = {
+    enable = mkEnableOption "Enable ProjectSend";
+
+    version = mkOption {
+      type = types.string;
+      default = "latest";
+      defaultText = "latest";
+      description = "Version name to use for ProjectSend images";
+    };
+
+    port = mkOption {
+      type = types.int;
+      default = 10006;
+      defaultText = "10006";
+      description = "Port to use for ProjectSend";
+    };
+
+    paths = {
+      default = mkOption {
+        type = types.path;
+        description = "Root path for ProjectSend (required)";
+      };
+
+      data = helpers.mkInheritedPathOption {
+        parentName = "paths.default";
+        parent = cfg.paths.default;
+        defaultSubpath = "data";
+        description = "Path for ProjectSend data (movies).";
+      };
+
+      config = helpers.mkInheritedPathOption {
+        parentName = "paths.default";
+        parent = cfg.paths.default;
+        defaultSubpath = "config";
+        description = "Path for ProjectSend config (config).";
+      };
+    };
+
+    admin-password = mkOption {
+      type = types.string;
+      default = "secret"; # REVIEW - maybe remove default to force user to specify
+      defaultText = "secret";
+      description = "Base password for ProjectSend admin user (change this!)";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+    virtualisation.oci-containers.backend = "docker";
+
+    virtualisation.oci-containers.containers = {
+      projectsend = {
+        image = "psitrax/projectsend:${cfg.version}";
+        ports = [ "${toString cfg.port}:80" ];
+        environment = {
+          PUID=1000;
+          PGID=1000;
+          TZ="Etc/UTC";
+        };
+        volumes = [
+          "${cfg.paths.data}:/data"
+          "${cfg.paths.config}:/config"
+        ];
+      };
+    };
+  };
+}
+
