402fdc0be84ab14deeb9b22014529438160078f6
Axel Denis <axel.denis@epitech.eu>
2025-10-08 09:56:30 +0000

    refactor: chibisafe

--- modules/chibisafe.nix
+++ modules/chibisafe.nix
@@ -38,82 +38,45 @@ let
   '';
 in
 {
-  options.control.chibisafe = {
-    enable = mkEnableOption "Enable chibisafe";
+  options.control.chibisafe =
+    (helpers.webServiceDefaults {
+      name = "Chibisafe";
+      version = "latest";
+      subdomain = "chibisafe";
+      port = 10004;
+    })
+    // {
 
-    version = mkOption {
-      type = types.str;
-      default = "latest";
-      defaultText = "latest";
-      description = "Version name to use for chibisafe images";
-    };
+      paths = {
+        default = helpers.mkInheritedPathOption {
+          parentName = "home server global default path";
+          parent = config.control.defaultPath;
+          defaultSubpath = "chibisafe";
+          description = "Root path for chibisafe media and appdata";
+        };
 
-    subdomain = mkOption {
-      type = types.str;
-      default = "chibisafe";
-      defaultText = "chibisafe";
-      description = "Subdomain to use for Chibisafe";
-    };
+        database = helpers.mkInheritedPathOption {
+          parentName = "paths.default";
+          parent = cfg.paths.default;
+          defaultSubpath = "database";
+          description = "Path for chibisafe database.";
+        };
 
-    port = mkOption {
-      type = types.int;
-      default = 10004;
-      defaultText = "10004";
-      description = "Port to use for chibisafe";
-    };
+        uploads = helpers.mkInheritedPathOption {
+          parentName = "paths.default";
+          parent = cfg.paths.default;
+          defaultSubpath = "uploads";
+          description = "Path for chibisafe uploads.";
+        };
 
-    forceLan = mkEnableOption ''
-      Force LAN access, ignoring router configuration.
-      You will be able to access this container on <lan_ip>:${toString cfg.port} regardless of your router configuration.
-    '';
-
-    lanOnly = mkEnableOption ''
-      Disable routing for this service. You will only be able to access it on your LAN.
-    '';
-
-    basicAuth = mkOption {
-      type = with types; attrsOf str;
-      default = { };
-      description = ''
-        If set, enable Nginx basic authentication for this service.
-        The value should be an attribute set of username-password pairs, e.g.
-        { user1 = "password1"; user2 = "password2"; }
-        Keep in mind that basic authentication works for web pages but can break dependant services (e.g. mobile apps).
-      '';
-    };
-
-    # ANCHOR - simple ctrl-shift-f insert for all webservices
-
-    paths = {
-      default = helpers.mkInheritedPathOption {
-        parentName = "home server global default path";
-        parent = config.control.defaultPath;
-        defaultSubpath = "chibisafe";
-        description = "Root path for chibisafe media and appdata";
-      };
-
-      database = helpers.mkInheritedPathOption {
-        parentName = "paths.default";
-        parent = cfg.paths.default;
-        defaultSubpath = "database";
-        description = "Path for chibisafe database.";
-      };
-
-      uploads = helpers.mkInheritedPathOption {
-        parentName = "paths.default";
-        parent = cfg.paths.default;
-        defaultSubpath = "uploads";
-        description = "Path for chibisafe uploads.";
-      };
-
-      logs = helpers.mkInheritedPathOption {
-        parentName = "paths.default";
-        parent = cfg.paths.default;
-        defaultSubpath = "logs";
-        description = "Path for chibisafe logs.";
+        logs = helpers.mkInheritedPathOption {
+          parentName = "paths.default";
+          parent = cfg.paths.default;
+          defaultSubpath = "logs";
+          description = "Path for chibisafe logs.";
+        };
       };
     };
-  };
 
   config = mkIf cfg.enable {
     virtualisation.docker.enable = true;
@@ -140,11 +103,7 @@ in
 
       chibisafe_caddy = {
         image = "caddy:2-alpine";
-        ports = [
-          "${
-            if (config.control.routing.lan || cfg.forceLan || cfg.lanOnly) then "" else "127.0.0.1:"
-          }${toString cfg.port}:80"
-        ];
+        ports = helpers.webServicePort config cfg;
         environment = {
           BASE_URL = ":80";
         };
