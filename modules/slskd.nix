1af1dc5551fc5c7b03c3ead311be18f776a2432b
Axel Denis <axel.denis@epitech.eu>
2025-11-21 23:40:59 +0100

    fix

--- modules/slskd.nix
+++ modules/slskd.nix
@@ -48,8 +48,7 @@ in {
     virtualisation.oci-containers.backend = "docker";
 
     assertions = [{
-      assertion = !(cfg.configuration ? SLSKD_SLSK_USERNAME && cfg.configuration
-        ? SLSKD_SLSK_PASSWORD);
+      assertion = false;
       message =
         "Please provide control.slskd.SLSKD_SLSK_USERNAME and control.slskd.SLSKD_SLSK_PASSWORD for slskd to start.";
     }];
