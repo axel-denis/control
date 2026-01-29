38f029ed3866ac92d2653a82c2ba94dc601ab613
Axel Denis <91873203+axel-denis@users.noreply.github.com>
2026-01-29 22:21:56 +0100

    Gitlab (#37)
    * draft: gitlab
    
    * feat: gitlab working (except ssh)
    
    * feat: gitlab docs

---
+++ modules/gitlab.nix
@@ -0,0 +1,83 @@
+{ config, helpers, lib, ... }:
+
+with lib;
+let cfg = config.control.gitlab;
+in {
+  options.control.gitlab = (helpers.webServiceDefaults {
+    name = "GitLab";
+    version = "latest";
+    subdomain = "gitlab";
+    port = 10012;
+  }) // {
+
+    ssh-port = lib.mkOption {
+      type = lib.types.int;
+      default = 44;
+      defaultText = toString 44;
+      description = "SSH port to use for GitLab";
+    };
+
+    paths = {
+      default = helpers.mkInheritedPathOption {
+        parentName = "home server global default path";
+        parent = config.control.defaultPath;
+        defaultSubpath = "gitlab";
+        description = "Root path for GitLab media and appdata";
+      };
+
+      config = helpers.mkInheritedPathOption {
+        parentName = "paths.default";
+        parent = cfg.paths.default;
+        defaultSubpath = "config";
+        description = "Path for GitLab config.";
+      };
+
+      logs = helpers.mkInheritedPathOption {
+        parentName = "paths.default";
+        parent = cfg.paths.default;
+        defaultSubpath = "logs";
+        description = "Path for GitLab logs.";
+      };
+
+      data = helpers.mkInheritedPathOption {
+        parentName = "paths.default";
+        parent = cfg.paths.default;
+        defaultSubpath = "data";
+        description = "Path for GitLab data";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+    virtualisation.oci-containers.backend = "docker";
+
+    # Creating directory with the user id asked by the container
+    systemd.tmpfiles.rules = [ "d ${cfg.paths.default} 0755 1000 1000" ];
+    virtualisation.oci-containers.containers = {
+      gitlab = {
+        image = "gitlab/gitlab-ce:${cfg.version}";
+        ports = (helpers.webServicePort config cfg 80) ++ ["${toString cfg.ssh-port}:22"];
+        extraOptions =
+          [ (mkIf config.control.updateContainers "--pull=always") "--shm-size=256m" ];
+        environment = {
+          GITLAB_OMNIBUS_CONFIG =
+            ''
+              external_url 'https://${cfg.subdomain}.${config.control.routing.domain}';
+              gitlab_rails['lfs_enabled'] = true;
+              gitlab_rails['gitlab_shell_ssh_port'] = ${toString cfg.ssh-port};
+              letsencrypt['enabled'] = false;
+              nginx['enable'] = true;
+              nginx['listen_port'] = 80;
+              nginx['listen_https'] = false;
+            '';
+        };
+        volumes = [ 
+          "${cfg.paths.config}:/etc/gitlab"
+          "${cfg.paths.logs}:/var/logs/gitlab"
+          "${cfg.paths.data}:/var/opt/gitlab"
+        ];
+      };
+    };
+  };
+}
