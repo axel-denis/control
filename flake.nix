27862f9817edaf3e3ade53385f840f62420ccaf6
Axel Denis <axel.denis@epitech.eu>
2026-09-10 11:51:13 +0200

    feat: updating to nixos 26.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Home Server Service Modules (aggregated)";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05"; };
 
   outputs = { self, nixpkgs, ... }:
     let
